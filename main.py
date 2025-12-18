3ffd080963a404b46ebc3d6af998ddb50a2b58c5
joyd58528 <joyd58528@gmail.com>
2025-12-18 11:57:03 +0530

    Changed the api key

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from openai import OpenAI
 
 client = OpenAI(
     # This is the default and can be omitted
-    api_key="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",)
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 response = client.responses.create(
     model="gpt-5-nano",
