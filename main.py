d94c6a479f9edc1104989e97f24a724f215e08c5
joyd58528 <joyd58528@gmail.com>
2025-12-18 15:08:17 +0530

    Can read the bill information

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
 from google import genai
 from google.genai import types
+from PIL import Image
 
+# client = genai.Client(api_key=os.environ.get('GEMINI_API_KEY'))
 client = genai.Client(api_key="AIzaSyA5BQwl-AsPmLrbxmBdugZNdMt4Ctu9E0o")
+path = input("Enter image path: ")
+img = Image.open(path)
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=types.Part.from_text(text='Why is the sky blue?'),
-    config=types.GenerateContentConfig(
+    model="gemini-2.5-flash",
+      contents=[
+        "Only provide the Date, Bill number and the total amount",
+        img
+    ],
+    config=genai.types.GenerateContentConfig(
         temperature=0,
         top_p=0.95,
         top_k=20,
-    ),
+    )
 )
 
 print(response.candidates[0].content.parts[0].text)
