39ddc7c9d6f7b30492742188600239a686c72378
joyd58528 <joyd58528@gmail.com>
2025-12-18 11:43:45 +0530

    Simple instruction to find the date

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from openai import OpenAI;
-model = "gpt-4.1"
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key="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",)
+
+response = client.responses.create(
+    model="gpt-5-nano",
+    # instructions="Some instructions",
+    input="What is the day?",
+)
+
+print(response.output_text)
