6bf1a93a1dd590ad08b69fbae776836b2d3ce8d5
joyd58528 <joyd58528@gmail.com>
2025-12-19 11:00:34 +0530

    working output

--- main.py
+++ main.py
@@ -8,27 +8,26 @@ st.set_page_config(page_title="Bill Generator", page_icon="🧾")
 st.title("Bill Generator test build")
 st.write("Upload an image")
 client = genai.Client(api_key=os.environ.get('GEMINI_API_KEY'))
-path = input("Enter image path: ")
-img = Image.open(path)
 
 uploaded_file = st.file_uploader(
     "Upload bill image",
     type=["jpg","webp", "jpeg", "png"]
 )
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-      contents=[
-        "Only provide the Bill number, Date, and the total amount provide it as an .json object. Do not include any other text. and also use camel case.",
-        img
-    ],
-    config=genai.types.GenerateContentConfig(
-        temperature=0,
-        top_p=0.95,
-        top_k=20,
+if uploaded_file:
+    st.image(uploaded_file, caption='Uploaded Image.')
+    image_bytes = uploaded_file.read()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            types.Part.from_text(text="Only provide the Bill number, Date, and the total amount provide it as an .json object. Do not include any other text. and also use camel case."),
+            types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg")
+        ],
+        config=genai.types.GenerateContentConfig(
+            temperature=0,
+            top_p=0.95,
+            top_k=20,
+        )
     )
-)
 
-keep = ["billNumber", "date", "totalAmount"]
-
-print(response.candidates[0].content.parts[0].text)
+    st.text(response.text)
